b077bf084f43bb16b55ee785ac98d265c0010aee
namelikonda <54454186+namelikonda@users.noreply.github.com>
2019-09-24 20:50:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace inClass0924
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
